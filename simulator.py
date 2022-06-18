352c03ba724762d1c7fd123d029c9f8c51da70f1
Ken Jiang <tian-hua.jiang@uwaterloo.ca>
2022-06-18 16:22:02 -0400

    create simulator

--- simulator.py
+++ simulator.py
@@ -1,9 +1,21 @@
-from board import Board
+from game import Game
 
 
-def play(player1, player2):
-    """
-    Plays game with player1 and player2
-    """
+class Simulator:
+    def __init__(self, player1, player2):
+        self.players = [player1, player2]
+        self.game = Game()
 
-    board = Board()
+    def play_game(self):
+        """
+        Plays game with player1 and player2
+        returns winner
+        """
+        to_play = 0
+        while True:
+            move = self.players[to_play].get_move(self.game)
+            result = self.game.do_move(move)
+            if result is not None:
+                return result
+            to_play = 1 - to_play
+            # flip board perspective

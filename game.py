cac6733f49c5c34a85d70b863773d9e48f6a62e6
maxjiang216 <76746545+maxjiang216@users.noreply.github.com>
2022-07-10 22:27:26 -0400

    Changed game.py for new move format

--- game.py
+++ game.py
@@ -49,11 +49,11 @@ class Game:
         # More than 2 lines, no legal moves
         if len(lines) > 2:
             return []
-        moves = []
+        moves = np.ones(96)
         # Need to break / extend lines
         if len(lines) > 0:
             for line in lines:
-                cur = []
+                cur = np.zeros(96)
                 if line[0][0] == "r":
                     target = int(line[0][1])
                     sources = []
@@ -71,55 +71,39 @@ class Game:
                         f = lambda x, y, z: x + 4 * y + z
                         if target > source:
                             f = lambda x, y, z: (48 - x) + 4 * (x - 1) + z
-                        cur.extend(
-                            [
-                                f(12, target, 1),
-                                f(12, target, 2),
-                                f(36, source, 1),
-                                f(36, source, 2),
-                            ]
-                        )
+                        cur[f(12, target, 1)] = 1
+                        cur[f(12, target, 2)] = 1
+                        cur[f(36, source, 1)] = 1
+                        cur[f(36, source, 2)] = 1
                         if line[0][2] == "l":
-                            cur.extend(
-                                [
-                                    f(12, target, 0),
-                                    f(36, source, 0),
-                                ]
-                            )
+                            cur[f(12, target, 0)] = 1
+                            cur[f(36, source, 0)] = 1
                             # Extend
                             if self.board.top(source, 3) == line[1]:
-                                cur.append(f(36, source, 3))
+                                cur[f(36, source, 3)] = 1
                         elif line[0][2] == "r":
-                            cur.extend(
-                                [
-                                    f(12, target, 3),
-                                    f(36, source, 3),
-                                ]
-                            )
+                            cur[f(12, target, 3)] = 1
+                            cur[f(36, source, 3)] = 1
                             # Extend
                             if self.board.top(source, 0) == line[1]:
-                                cur.append(f(36, source, 0))
+                                cur[f(36, source, 0)] = 1
                     if line[0][2] == "l":
-                        cur.append(target * 3 + 2)
+                        cur[target * 3 + 2] = 1
                     elif line[0][2] == "r":
-                        cur.append(24 + target * 3)
+                        cur[24 + target * 3] = 1
                     # Place moves, not on capitals
                     if line[1] < 2:
-                        cur.extend(
-                            [
-                                48 + (line[1] + 1) * 16 + target * 4 + 1,
-                                48 + (line[1] + 1) * 16 + target * 4 + 2,
-                            ]
-                        )
+                        cur[48 + (line[1] + 1) * 16 + target * 4 + 1] = 1
+                        cur[48 + (line[1] + 1) * 16 + target * 4 + 2] = 1
                         if line[0][2] == "l":
-                            cur.append(48 + (line[1] + 1) * 16 + target * 4)
+                            cur[48 + (line[1] + 1) * 16 + target * 4] = 1
                         elif line[0][2] == "r":
-                            cur.append(48 + (line[1] + 1) * 16 + target * 4 + 3)
+                            cur[48 + (line[1] + 1) * 16 + target * 4 + 3] = 1
                     # Place capital, extend
                     if line[0][2] == "l":
-                        cur.append(48 + line[1] * 16 + target * 4 + 3)
+                        cur[48 + line[1] * 16 + target * 4 + 3] = 1
                     elif line[0][2] == "r":
-                        cur.append(48 + line[1] * 16 + target * 4)
+                        cur[48 + line[1] * 16 + target * 4] = 1
                     target = int(line[0][1])
                     sources = []
                     if target == 0:
@@ -136,201 +120,144 @@ class Game:
                         f = lambda x, y, z: x + 3 * y + z
                         if target > source:
                             f = lambda x, y, z: (24 - x) + 3 * y + (z - 1)
-                        cur.extend(
-                            [
-                                f(0, 1, target),
-                                f(0, 2, target),
-                                f(24, 1, source),
-                                f(24, 2, source),
-                            ]
-                        )
+                        cur[f(0, 1, target)] = 1
+                        cur[f(0, 2, target)] = 1
+                        cur[f(24, 1, source)] = 1
+                        cur[f(24, 2, source)] = 1
                         if line[0][2] == "u":
-                            cur.extend(
-                                [
-                                    f(0, 0, target),
-                                    f(24, 0, source),
-                                ]
-                            )
+                            cur[f(0, 0, target)] = 1
+                            cur[f(24, 0, source)] = 1
                             # Extend
                             if self.board.top(3, source) == line[1]:
-                                cur.append(f(24, 3, source))
+                                cur[f(24, 3, source)] = 1
                         elif line[0][2] == "d":
-                            cur.extend(
-                                [
-                                    f(0, 3, target),
-                                    f(24, 3, source),
-                                ]
-                            )
+                            cur[f(0, 3, target)] = 1
+                            cur[f(24, 3, source)] = 1
                             # Extend
                             if self.board.top(0, source) == line[1]:
-                                cur.append(f(24, 0, source))
+                                cur[f(24, 0, source)] = 1
                     if line[0][2] == "u":
-                        cur.append(12 + 2 * 3 + target)
+                        cur[12 + 2 * 3 + target] = 1
                     elif line[0][2] == "r":
-                        cur.append(36 + 0 * 3 + target)
+                        cur[36 + 0 * 3 + target] = 1
                     # Place moves, not on capitals
                     if line[1] < 2:
-                        cur.extend(
-                            [
-                                48 + 16 * (line[1] + 1) + 1 * 3 + target,
-                                48 + (line[1] + 1) * 16 + 2 * 3 + target,
-                            ]
-                        )
+                        cur[48 + 16 * (line[1] + 1) + 1 * 3 + target] = 1
+                        cur[48 + (line[1] + 1) * 16 + 2 * 3 + target] = 1
                         if line[0][2] == "u":
-                            cur.append(48 + (line[1] + 1) * 16 + 0 * 3 + target)
+                            cur[48 + (line[1] + 1) * 16 + 0 * 3 + target] = 1
                         elif line[0][2] == "d":
-                            cur.append(48 + (line[1] + 1) * 16 + 3 * 3 + target)
+                            cur[48 + (line[1] + 1) * 16 + 3 * 3 + target] = 1
                     # Place capital, extend
                     if line[0][2] == "u":
-                        cur.append(48 + line[1] * 16 + 3 * 3 + target)
+                        cur[48 + line[1] * 16 + 3 * 3 + target] = 1
                     elif line[0][2] == "d":
-                        cur.append(48 + line[1] * 16 + 0 * 3 + target)
+                        cur[48 + line[1] * 16 + 0 * 3 + target] = 1
                 elif line[0][0] == "d":
                     # Flip column if necessary
                     f = lambda x, y, z: x + y * 3 + z
+                    g = lambda x: x
                     s = lambda x: x
                     if line[0][1] == "1":
-                        f = lambda x: (24 - x) + y * 3 + ((3 - x) - 1)
+                        f = lambda x, y, z: (24 - x) + y * 3 + (3 - z)
+                        g = lambda x: 3 - x
                         s = lambda x: 3 - x
                     # Move moves
-                    cur.extend(
-                        [
-                            36 + 1 * 4 + s(1),
-                            f(0, 1, 1),
-                            12 + 1 * 4 + s(1),
-                            f(24, 1, 1),
-                            12 + 0 * 4 + s(1),
-                            f(24, 1, 2),
-                            36 + 2 * 4 + s(1),
-                            f(0, 1, 0),
-                        ]
-                    )
+                    cur[36 + 1 * 4 + s(1)] = 1
+                    cur[f(0, 1, 1)] = 1
+                    cur[12 + 1 * 4 + s(1)] = 1
+                    cur[f(24, 1, 1)] = 1
+                    cur[12 + 0 * 4 + s(1)] = 1
+                    cur[f(24, 1, 2)] = 1
+                    cur[36 + 2 * 4 + s(1)] = 1
+                    cur[f(0, 1, 0)] = 1
                     if line[0][2] == "u":
-                        cur.extend(
-                            [
-                                Move(False, 0, f(0), 0, 0, f(1)),
-                                Move(False, 0, f(0), 0, 1, f(0)),
-                                Move(False, 0, f(1), 0, 0, f(0)),
-                                Move(False, 1, f(0), 0, 0, f(0)),
-                            ]
-                        )
+                        cur[f(0, 0, 0)] = 1
+                        cur[12 + 0 * 4 + s(0)] = 1
+                        cur[f(24, 0, 1)] = 1
+                        cur[36 + 1 * 4 + s(0)] = 1
                         # Extend
-                        if self.board.top(2, f(3)) == line[1]:
-                            cur.append(Move(False, 2, f(3), 0, 3, f(3)))
-                        if self.board.top(3, f(2)) == line[1]:
-                            cur.append(Move(False, 3, f(2), 0, 3, f(3)))
+                        if self.board.top(2, g(3)) == line[1]:
+                            cur[12 + 2 * 4 + s(3)] = 1
+                        if self.board.top(3, g(2)) == line[1]:
+                            cur[f(12, 3, 2)] = 1
                     elif line[0][2] == "d":
-                        cur.extend(
-                            [
-                                Move(False, 3, f(3), 0, 2, f(3)),
-                                Move(False, 3, f(3), 0, 3, f(2)),
-                                Move(False, 2, f(3), 0, 3, f(3)),
-                                Move(False, 3, f(2), 0, 3, f(3)),
-                            ]
-                        )
+                        cur[36 + 3 * 4 + s(3)] = 1
+                        cur[f(24, 3, 3)] = 1
+                        cur[12 + 2 * 4 + s(3)] = 1
+                        cur[f(0, 3, 2)] = 1
                         # Extend
-                        if self.board.top(0, f(1)) == line[1]:
-                            cur.append(Move(False, 0, f(1), 0, 0, f(0)))
+                        if self.board.top(0, g(1)) == line[1]:
+                            cur[f(24, 0, 1)] = 1
                         if self.board.top(1, f(0)) == line[1]:
-                            cur.append(Move(False, 1, f(0), 0, 0, f(0)))
+                            cur[f(0, 1, 0)] = 1
                     # Place moves, not capitals
                     if line[1] < 2:
-                        cur.extend(
-                            [
-                                Move(True, 1, f(1), line[1] + 1),
-                                Move(True, 2, f(2), line[1] + 1),
-                            ]
-                        )
+                        cur[48 + (line[1] + 1) * 16 + 1 * 4 + s(1)] = 1
+                        cur[48 + (line[1] + 1) * 16 + 2 * 4 + s(2)] = 1
                         if line[0][2] == "u":
-                            cur.append(Move(True, 0, f(0), line[1] + 1))
+                            cur[48 + (line[1] + 1) * 16 + 1 * 0 + s(0)] = 1
                         elif line[0][2] == "d":
-                            cur.append(Move(True, 3, f(3), line[1] + 1))
+                            cur[48 + (line[1] + 1) * 16 + 3 * 4 + s(3)] = 1
                     # Place capital, extend
                     if line[0][2] == "u":
-                        cur.append(Move(True, 3, f(3), line[1]))
+                        cur[48 + line[1] * 16 + 3 * 4 + s(3)] = 1
                     elif line[0][2] == "d":
-                        cur.append(Move(True, 0, f(0), line[1]))
+                        cur[48 + line[1] * 16 + 3 * 0 + s(0)] = 1
                 # Short diagonals
                 else:
-                    # Flip column if necessary
-                    f = lambda x: x
+                    # Flip coordinates if necessary
+                    f = lambda x, y, z: x + y * 3 + z
+                    g = lambda x: x
+                    s = lambda x, y, z: x + y * 4 + z
+                    t = lambda x: x
                     if line[0][1] in ["1", "2"]:
-                        f = lambda x: 3 - x
-                    # Shift down if necessary
-                    s = lambda x: x
+                        f = lambda x, y, z: (24 - x) + y * 3 + (3 - z)
+                        g = lambda x: 3 - x
                     if line[0][1] in ["2", "3"]:
-                        s = lambda x: 3 - x
+                        s = lambda x, y, z: (48 - x) + (3 - y) * 4 + z
+                        t = lambda x: 3 - x
                     # Move moves
-                    cur.extend(
-                        [
-                            Move(False, s(0), f(3), 0, s(0), f(2)),
-                            Move(False, s(1), f(2), 0, s(0), f(2)),
-                            Move(False, s(0), f(1), 0, s(0), f(2)),
-                            Move(False, s(0), f(1), 0, s(1), f(1)),
-                            Move(False, s(1), f(2), 0, s(1), f(1)),
-                            Move(False, s(2), f(1), 0, s(1), f(1)),
-                            Move(False, s(1), f(0), 0, s(1), f(1)),
-                            Move(False, s(1), f(0), 0, s(2), f(0)),
-                            Move(False, s(2), f(1), 0, s(2), f(0)),
-                            Move(False, s(0), f(2), 0, s(0), f(3)),
-                            Move(False, s(0), f(2), 0, s(1), f(2)),
-                            Move(False, s(0), f(2), 0, s(0), f(1)),
-                            Move(False, s(1), f(1), 0, s(0), f(1)),
-                            Move(False, s(1), f(1), 0, s(1), f(2)),
-                            Move(False, s(1), f(1), 0, s(2), f(1)),
-                            Move(False, s(1), f(1), 0, s(1), f(0)),
-                            Move(False, s(2), f(0), 0, s(1), f(0)),
-                            Move(False, s(2), f(0), 0, s(2), f(1)),
-                        ]
-                    )
+                    cur[f(24, t(0), g(3))] = 1
+                    cur[s(36, t(1), g(2))] = 1
+                    cur[f(0, t(0), g(1))] = 1
+                    cur[s(12, t(0), g(1))] = 1
+                    cur[f(24, t(1), g(2))] = 1
+                    cur[s(36, t(2), g(1))] = 1
+                    cur[f(0, t(1), g(0))] = 1
+                    cur[s(12, t(1), g(0))] = 1
+                    cur[f(24, t(2), g(1))] = 1
+                    cur[f(0, t(0), g(2))] = 1
+                    cur[s(12, t(0), g(2))] = 1
+                    cur[f(24, t(0), g(2))] = 1
+                    cur[s(36, t(1), g(1))] = 1
+                    cur[f(0, t(1), g(1))] = 1
+                    cur[s(12, t(1), g(1))] = 1
+                    cur[f(24, t(1), g(1))] = 1
+                    cur[s(36, t(2), g(0))] = 1
+                    cur[f(0, t(2), g(0))] = 1
                     if line[0][1] in ["2", "3"]:
-                        cur.extend(
-                            [
-                                Move(False, 1, f(2), 0, 0, f(2)),
-                                Move(False, 0, f(2), 0, 1, f(2)),
-                            ]
-                        )
+                        cur[36 + 1 * 4 + g(2)] = 1
+                        cur[12 + 0 * 4 + g(2)] = 1
                     else:
-                        cur.extend(
-                            [
-                                Move(False, 3, f(0), 0, 2, f(0)),
-                                Move(False, 2, f(0), 0, 3, f(0)),
-                            ]
-                        )
+                        cur[36 + 3 * 4 + g(0)] = 1
+                        cur[12 + 2 * 4 + g(0)] = 1
                     # Place moves, not capitals
                     if line[1] < 2:
-                        cur.extend(
-                            [
-                                Move(True, s(0), f(2), line[1] + 1),
-                                Move(True, s(1), f(1), line[1] + 1),
-                                Move(True, s(2), f(0), line[1] + 1),
-                            ]
-                        )
+                        cur[48 + (line[1] + 1) * 16 + t(0) * 4 + g(2)] = 1
+                        cur[48 + (line[1] + 1) * 16 + t(1) * 4 + g(1)] = 1
+                        cur[48 + (line[1] + 1) * 16 + t(2) * 4 + g(0)] = 1
                 # First line
                 if len(moves) == 0:
                     moves = cur
                 else:
-                    moves = list(set(moves) & set(cur))
-        else:
-            # Place
-            for ptype in range(3):
-                if self.pieces[self.to_play][ptype] == 0:
-                    continue
-                for row in range(4):
-                    for col in range(4):
-                        moves.append(Move(True, row, col, ptype))
-            # Move
-            for row1 in range(4):
-                for row2 in range(4):
-                    if abs(row1 - row2) > 1:
-                        continue
-                    for col1 in range(4):
-                        for col2 in range(4):
-                            moves.append(Move(False, row1, col1, 0, row2, col2))
-        legal_moves = []
-        for move in moves:
-            if self.is_legal(move):
-                legal_moves.append(move)
+                    for i, move in enumerate(moves):
+                        if not move:
+                            moves[i] = 0
+        legal_moves = np.zeros(96)
+        for i, move in enumerate(moves):
+            if move and self.is_legal(Move(i)):
+                legal_moves[i] = 1
         return legal_moves
 
     def do_move(self, move):

42774bc965cc4c0ca2acc555e73e0cb1b45e4504
Max Jiang <76746545+maxjiang216@users.noreply.github.com>
2022-10-16 17:25:33 -0400

    Add unary constructor

--- cpp/move.cpp
+++ cpp/move.cpp
@@ -10,31 +10,50 @@ Move::Move(int ptype, int row, int col): mtype{true}, ptype{ptype}, row1{row}, c
 Move::Move(int row1, int col1, int row2, int col2): mtype{false}, row1{row1}, col1{col1}, row2{row2}, col2{col2} {}
 
 // Convert move_id to Move object
-Move get_move_from_id(int move_id) {
+Move::Move(int move_id) {
 
     // Place
     // I believe that place is more common
     if (move_id >= 48) {
-	return Move{(move_id - 48) / 16, (move_id % 16) / 4, move_id % 4};
+	ptype = (move_id - 48) / 16;
+	row1 = (move_id % 16) / 4;
+	col1 = move_id % 4;
     }
 
     // Move
 
     // Right
-    if (move_id < 12)
-        return Move{move_id / 3, move_id % 3, move_id / 3, move_id % 3 + 1};
-
+    else if (move_id < 12) {
+	row1 = move_id / 3;
+	col1 = move_id % 3;
+	row2 = move_id / 3;
+	col2 = move_id % 3 + 1;
+    }
+	
     // Down
-    if (move_id < 24)
-	return Move{(move_id - 12) / 4, move_id % 4, (move_id - 8) / 4, move_id % 4};
+    else if (move_id < 24) {
+        row1 = (move_id - 12) / 4;
+	col1 = move_id % 4;
+	row2 = (move_id - 8) / 4;
+	col2 = move_id % 4;
+    }
 
     // Left
-    if (move_id < 36)
-	return Move{(move_id - 24) / 3, move_id % 3 + 1, (move_id - 24) / 3, move_id % 3};
+    else if (move_id < 36) {
+	row1 = (move_id - 24) / 3;
+	col1 = move_id % 3 + 1;
+	row2 = (move_id - 24) / 3;
+	col2 = move_id % 3;
+    }
 
     // Up
-    return Move{(move_id - 32) / 4, move_id % 4, (move_id - 36) / 4, move_id % 4};
-
+    else {
+        row1 = (move_id - 32) / 4;
+	col1 = move_id % 4;
+	row2 = (move_id - 36) / 4;
+	col2 = move_id % 4;
+    }
+	
 }
 
 // Convert place move to int

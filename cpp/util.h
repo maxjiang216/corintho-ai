2b2eca80316a2b78783d00e747ebd84732bf239c
maxjiang216 <76746545+maxjiang216@users.noreply.github.com>
2022-11-19 15:26:18 -0500

    uniformize uints

--- cpp/util.h
+++ cpp/util.h
@@ -13,27 +13,24 @@ using std::string;
 
 // Default ints to use
 // Generally memory is not too critical, prioritize speed
-typedef int_fast8_t int8;
-typedef uint_fast8_t uint8;
-typedef uint_fast16_t uint16;
-typedef uint_fast32_t uint32;
+typedef uint_fast32_t uint;
+typedef int_fast32_t fint;
 
 // Ints for memory critical parts
 // Generally use in large arrays
 typedef uint_least8_t uint8s;
 typedef uint_least16_t uint16s;
 
-
 // Define constants used by the pipeline
 // This is for constants used by more than one file
 // Otherwise keep the constants in the file that uses it
 
 // Size of game state sample
-const uint8 GAME_STATE_SIZE = 70;
+const uint GAME_STATE_SIZE = 70;
 // Number of possible legal moves
-const uint8 NUM_TOTAL_MOVES = 96;
+const uint NUM_TOTAL_MOVES = 96;
 // Number of moves the simulator uses
-const uint8 NUM_MOVES = 56;
+const uint NUM_MOVES = 56;
 
 enum Result {NONE, LOSS, DRAW, WIN};
 

a951dc2724acd3ce7d79fc9d14ee78eb72e82dbc
maxjiang216 <76746545+maxjiang216@users.noreply.github.com>
2022-11-27 15:06:14 -0500

    minor change

--- startup_script.sh
+++ startup_script.sh
@@ -1,11 +1,13 @@
 sudo apt-get update
 sudo apt install python3-pip -y # 328MB
 sudo apt-get install git -y # 39MB
+sudo apt-get clang-format -y
 pip install numpy # 17MB
 pip install keras==2.9 # 5MB
 pip install tf-nightly # 588MB
 pip install cython # 5MB
 pip install toml
+pip install black
 export PATH="~/.local/bin:$PATH"
 git clone https://github.com/maxjiang216/corintho-ai.git
 cd corintho-ai

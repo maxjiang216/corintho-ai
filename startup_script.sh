0d82137432b1d6db46fb2613e8cf8f7945e866a7
Max Jiang <76746545+maxjiang216@users.noreply.github.com>
2022-12-09 15:31:03 -0500

    Update startup_script.sh

--- startup_script.sh
+++ startup_script.sh
@@ -2,6 +2,7 @@ sudo apt-get update
 sudo apt install python3-pip -y # 328MB
 sudo apt-get install git -y # 39MB
 sudo apt-get clang-format -y
+sudo apt-get dos2linux -y
 pip install numpy # 17MB
 pip install keras==2.9 # 5MB
 pip install tf-nightly # 588MB
@@ -11,5 +12,6 @@ pip install black
 export PATH="~/.local/bin:$PATH"
 git clone https://github.com/maxjiang216/corintho-ai.git
 cd corintho-ai
+dos2linux *
 chmod +x train.sh
 nohup ~/corintho-ai/train.sh

bf3b94c7d118048a11b17bef070fd25526f82649
maxjiang216 <76746545+maxjiang216@users.noreply.github.com>
2022-12-19 19:12:39 -0500

    clean up trainer

--- cpp/trainer.h
+++ cpp/trainer.h
@@ -12,30 +12,9 @@
 
 class Trainer {
 
-  // Number of Nodes to allocate together
-  // Determine best number for this empirically
-  // This will be deprecated once we use Boost pools
-  static const uintf BLOCK_SIZE = 8192;
+  // SelfPlayer objects
+  std::vector<SelfPlayer *> games;
 
-  // Determine best starting size empirically (per game)
-  static const uintf HASH_TABLE_SIZE = 5003;
-
-  // Prime multiplicative factor used in hashing child from parent node
-  // We can tinker with this constant to minimize collisions
-  static const uintf MULT_FACTOR = 10009;
-  // Additive factor to space children of the same parent node
-  // We don't want children to be close to avoid clustering, but they do not
-  // need to be too far Average capacity <LOAD_FACTOR should probabilistically
-  // prevent long clusters Largest prime less than HASH_TABLE_SIZE / NUM_MOVES,
-  // so that we never wrap around Guarantees no collision amongst siblings as
-  // long as probe length is sufficiently short
-  static const uintf ADD_FACTOR = 23;
-
-  // Load factor for hash table rehashing
-  static constexpr float LOAD_FACTOR = 0.75;
-
-  // Number of games to play
-  uintf num_games;
   // Number of iterations per move (used to compute offsets)
   uintf num_iterations;
   // Counter used to keep track of number of iterations done for offsets
@@ -43,25 +22,8 @@ class Trainer {
   // Keep track of how many games are done
   uintf games_done;
 
-  // SelfPlayer objects
-  std::vector<SelfPlayer *> games;
   // Track which games are done
   std::vector<bool> is_done;
-
-  // We use a hash table to store the nodes
-  std::vector<Node *> hash_table;
-  // Keep track of which nodes are stale
-  std::vector<bool> is_stale;
-
-  // Blocks used to allocate in chunk, saves on allocation cost
-  // Eventually this should be replaced with Boost pools
-  Node *cur_block;
-  // Index of first available node in current block
-  uintf cur_ind;
-  // List of blocks that have been allocated so we can delete them appropriately
-  std::vector<Node *> blocks;
-
-  std::string logging_folder;
 
   // Random generator for all operations
   std::mt19937 generator;
@@ -69,22 +31,6 @@ class Trainer {
   // Initialize SelfPlayers (factored out of different version of constructor)
   void initialize(bool testing, uintf num_games, uintf num_logged, float c_puct,
                   float epsilon, const std::string &logging_folder);
-
-  // Hash child nodes
-  static uintf hash(uintf seed, uintf move_choice);
-
-  // Place root
-  void place(uintf pos, uintf seed);
-  void place(uintf pos, uintf seed, const Game &game, uintf depth);
-  // Place node
-  void place(uintf pos, uintf seed, const Game &game, uintf depth, uintf parent,
-             uintf move_choice);
-
-  // Stale all nodes in the queue and their descendents
-  void stale_all(std::queue<uintf> &nodes);
-
-  // Rehash
-  void rehash();
 
 public:
   // Training
@@ -99,42 +45,12 @@ public:
 
   // Main function that will be called by Cython
   // Training version
-  bool do_iteration(const float evaluations[], const float probabilities[],
+  bool do_iteration(float evaluations[], float probabilities[],
                     float game_states[]);
   // Testing version
-  bool do_iteration(const float evaluations_1[], const float probabilities_1[],
-                    const float evaluations_2[], const float probabilities_2[],
+  bool do_iteration(float evaluations_1[], float probabilities_1[],
+                    float evaluations_2[], float probabilities_2[],
                     float game_states[]);
-
-  // Place root in hash table (random hash)
-  // Place starting position
-  uintf place_root();
-  // Place a given position
-  // This is used when receiving an opponent move the player did not search
-  // (rare)
-  uintf place_root(const Game &game, uintf depth);
-
-  // Place child node in hash table
-  uintf place_next(uintf seed, const Game &game, uintf depth, uintf parent,
-                   uintf move_choice);
-  // Find the child node
-  uintf find_next(uintf seed, uintf move_choice) const;
-
-  // Get the pointer to a Node given the index
-  Node *get_node(uintf id) const;
-
-  // Stale nodes
-  // move_down used normally when a move has been chosen
-  void move_down(uintf root, uintf move_choice);
-  // delete_tree is used when the game ends
-  // or when moving to a move not searched
-  void delete_tree(uintf root);
-
-  // Call occasionally by Cython to rehash if too filled
-  void rehash_if_full();
-
-  // used by other classes to generate random numbers
-  uintf generate();
 
   // Counts the number of samples in all the games
   uintf count_samples() const;

ebef9ecb363979d3d1d819bb658fa3340947f265
Max Jiang <76746545+maxjiang216@users.noreply.github.com>
2022-12-24 16:03:30 -0500

    Update startup_script.sh

--- startup_script.sh
+++ startup_script.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 sudo apt install python3-pip -y # 328MB
 sudo apt-get install git -y # 39MB
 sudo apt-get clang-format -y
-sudo apt-get dos2linux -y
+sudo apt-get dos2unix -y
 pip install numpy # 17MB
 pip install keras==2.9 # 5MB
 pip install tf-nightly # 588MB

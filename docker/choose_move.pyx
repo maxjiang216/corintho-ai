c94b20168bb047d1870b9e13d445d6b7bf8ef0af
maxjiang216 <maxjiang216@gmail.com>
2023-05-02 18:27:36 -0400

    starting ai service

---
+++ docker/choose_move.pyx
@@ -0,0 +1,31 @@
+from play_corintho import get_move
+
+def choose_move(
+        game_state,
+        time_limit,
+        max_searches=0,
+        num_threads=0,
+        max_nodes=0,
+    ):
+    """
+    Use MCST and neural network to choose a move.
+    game_state is dictionary of game state
+    time_limit is time limit in seconds
+    max_searches is maximum number of searches per evaluation (0 for no limit)
+    num_threads is number of threads to use (0 for no limit)
+    max_nodes is maximum number of nodes to search (0 for no limit)
+    """
+
+    # Construct a MCST object
+    # Do first search
+    # Loop until time limit is reached
+    # Evaluate with neural network
+    # Do more searches
+
+    # Choose the best move
+    # Get a list of all legal moves
+    # Get a list of winning moves
+    # Get a list of drawing moves
+    # Return state of game if terminal
+
+    return None

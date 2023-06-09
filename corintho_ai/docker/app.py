653197a1b1ea8fea0b2b455e9628980a2f3231a0
maxjiang216 <maxjiang216@gmail.com>
2023-06-09 10:06:41 -0400

    reformat docker/app.py

--- corintho_ai/docker/app.py
+++ corintho_ai/docker/app.py
@@ -7,9 +7,10 @@ app = Flask(__name__)
 CORS(app)
 
 # Load the TFLite model
-tflite_model_path = 'tflite_model.tflite'
+tflite_model_path = "tflite_model.tflite"
 interpreter = tf.lite.Interpreter(model_path=tflite_model_path)
 interpreter.allocate_tensors()
+
 
 @app.route("/choose_move", methods=["POST"])
 def choose_move_route():
@@ -20,13 +21,15 @@ def choose_move_route():
             data["timeLimit"],
             data["searchesPerEval"],
             data["maxNodes"],
-            interpreter
+            interpreter,
         )
     )
+
 
 @app.route("/warm_up", methods=["POST"])
 def warm_up():
     return jsonify({"status": 200})
+
 
 if __name__ == "__main__":
     app.run(debug=True)

9d73f74e73f3eecfd6043836fff8830d626afca2
maxjiang216 <maxjiang216@gmail.com>
2023-06-09 22:54:18 -0400

    lint

--- tests/cpp/selfplayer_test.cpp
+++ tests/cpp/selfplayer_test.cpp
@@ -5,18 +5,18 @@ using namespace std;
 
 // Test the training constructor
 TEST(SelfPlayerTest, TrainingConstructor) {
-    std::mt19937 *generator = new std::mt19937;
-    SelfPlayer selfplayer(1, generator);
-    
-    EXPECT_EQ(selfplayer.count_samples(), 0);
-    cerr << "12";
+  std::mt19937 *generator = new std::mt19937;
+  SelfPlayer selfplayer(1, generator);
+
+  EXPECT_EQ(selfplayer.count_samples(), 0);
+  cerr << "12";
 }
 
 // Test do first iteration
 TEST(SelfPlayerTest, DoFirstIteration) {
-    std::mt19937 *generator = new std::mt19937;
-    SelfPlayer selfplayer(1, generator);
+  std::mt19937 *generator = new std::mt19937;
+  SelfPlayer selfplayer(1, generator);
 
-    selfplayer.do_first_iteration();
-    EXPECT_EQ(selfplayer.count_requests(), 1);
+  selfplayer.do_first_iteration();
+  EXPECT_EQ(selfplayer.count_requests(), 1);
 }

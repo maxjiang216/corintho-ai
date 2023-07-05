a55fb061f17e8323f6e817ffdc2e5bfec30be6a6
maxjiang216 <t63jiang@uwaterloo.ca>
2023-07-05 07:21:04 -0400

    Add mainpage to Doxygen

---
+++ corintho_ai/cpp/include/mainpage.h
@@ -0,0 +1,9 @@
+/*!
+ * \mainpage Corintho AI Documentation
+ * 
+ * Documentation generated with Doxygen.
+ * 
+ * View the source code on GitHub: https://github.com/maxjiang216/corintho-ai
+ *  
+ * etc...
+ */

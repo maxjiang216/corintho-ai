feab32807a458be69e00680ac704b738d0139724
maxjiang216 <maxjiang216@users.noreply.github.com>
2023-07-09 17:20:10 +0000

    deploy: 4312c110e3787d74ac61281c73e8c8a0028ce8dc

--- search/all_f.js
+++ search/all_f.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['terminal_101',['terminal',['../classNode.html#a69ba9a98acd8c22992e64e4fdcda109b',1,'Node']]],
-  ['testing_5f_102',['testing_',['../classTrainMC.html#a036f410bddafcbf51484cab2e53f0468',1,'TrainMC']]],
+  ['testing_5f_102',['testing_',['../classSelfPlayer.html#ac2a75fc5f8a6d89321b9d1206c025f12',1,'SelfPlayer::testing_()'],['../classTrainMC.html#a036f410bddafcbf51484cab2e53f0468',1,'TrainMC::testing_()']]],
   ['to_5feval_5f_103',['to_eval_',['../classSelfPlayer.html#a7b7934ea9ffe4746612b22c30d6a5293',1,'SelfPlayer::to_eval_()'],['../classTrainMC.html#a15a07e62254af9d40da5b32b047fdf8a',1,'TrainMC::to_eval_()']]],
   ['to_5fplay_5f_104',['to_play_',['../classGame.html#a214707f5e2f6cad01717fd7cab022c43',1,'Game::to_play_()'],['../classSelfPlayer.html#aa08c551354d38e2586fb28fbad0eb491',1,'SelfPlayer::to_play_()']]],
   ['top_105',['top',['../classGame.html#a1e9a19ad31f4d16e2844aba23b82d354',1,'Game']]],

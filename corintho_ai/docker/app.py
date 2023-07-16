9667fc56d79dd1f5684818f8b06bd865a71836d2
Max Jiang <76746545+maxjiang216@users.noreply.github.com>
2023-07-16 11:22:49 -0400

    Install Flask requirements in Run Stage (#147)
    * Clean up Dockerfile
    
    * Install Flask requirements in run stage

--- corintho_ai/docker/app.py
+++ corintho_ai/docker/app.py
@@ -1,15 +1,9 @@
-import tensorflow as tf
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from play_corintho import choose_move
 
 app = Flask(__name__)
 CORS(app)
-
-# Load the TFLite model
-_TFLITE_MODEL_PATH = "corintho_ai/docker/tflite_model.tflite"
-model = tf.lite.Interpreter(model_path=_TFLITE_MODEL_PATH)
-model.allocate_tensors()
 
 
 @app.route("/choose_move", methods=["POST"])
@@ -19,7 +13,6 @@ def choose_move_route():
         choose_move(
             data["gameState"],
             data["timeLimit"],
-            model,
             data["searchesPerEval"],
             data["maxNodes"],
         )

61e9b9d8e3c8d3e6ffbd2b58e071d7a5e8bfaff7
maxjiang216 <maxjiang216@users.noreply.github.com>
2023-07-16 14:37:18 +0000

    deploy: da406d6199134728b099678f1d7038c752077860

--- search/all_3.js
+++ search/all_3.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['denominator_5f_27',['denominator_',['../classNode.html#a0d36333d8ee9106f4c0adfd6894f73fa',1,'Node']]],
   ['depth_5f_28',['depth_',['../classNode.html#a572e07f04e74e67257534830a3a00800',1,'Node']]],
-  ['dockermc_29',['DockerMC',['../classDockerMC.html',1,'DockerMC'],['../classDockerMC.html#a965ad6d8f6445ad3303c4da3c20db246',1,'DockerMC::DockerMC(int32_t max_searches, int32_t searches_per_eval, float c_puct, float epsilon, int32_t board[4 *kBoardSize], int32_t to_play, int32_t pieces[6])']]],
+  ['dockermc_29',['DockerMC',['../classDockerMC.html',1,'DockerMC'],['../classDockerMC.html#a52120952903e8b5674caca60e690a48f',1,'DockerMC::DockerMC(int32_t seed, int32_t max_searches, int32_t searches_per_eval, float c_puct, float epsilon, int32_t board[4 *kBoardSize], int32_t to_play, int32_t pieces[6])']]],
   ['doiteration_30',['doIteration',['../classDockerMC.html#ad2128c3e0b3e717edf11c4096ca9efc2',1,'DockerMC::doIteration()'],['../classSelfPlayer.html#ac0c870319e63419b551b71b0b2bc228a',1,'SelfPlayer::doIteration()'],['../classTrainer.html#af7012e727c7b84b0a41706bc05fea1e7',1,'Trainer::doIteration()'],['../classTrainMC.html#a82b86f0869b129e0a5ec7459406a8083',1,'TrainMC::doIteration()']]],
   ['domove_31',['doMove',['../classGame.html#a9c42c87dacf3e39b7c1b315c320b47a1',1,'Game']]],
   ['done_32',['done',['../classDockerMC.html#a1c30b6ceaa604e86c1129ffdc804e3e8',1,'DockerMC::done()'],['../classTrainMC.html#aeb6398737459f4a8e20b4724171049e6',1,'TrainMC::done()']]],

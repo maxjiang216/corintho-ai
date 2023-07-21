d3066316f87d8e1ec2ccfdb865fc5b23a3fff2b0
maxjiang216 <maxjiang216@gmail.com>
2023-07-21 18:28:03 -0400

    move train folder

--- scripts/bash/train.sh
+++ scripts/bash/train.sh
@@ -1,2 +1,2 @@
-bash bash/build.sh
-python3 python/main.py python/wrapper.py -p toml/train.toml -n ${1:-1}
+bash scripts/bash/build.sh
+python3 corintho_ai/python/main.py corintho_ai/python/wrapper.py -p corintho_ai/toml/train.toml -n ${1:-1}

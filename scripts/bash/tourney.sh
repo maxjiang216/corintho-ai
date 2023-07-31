88f9b775bf11be55ba58b56f9f8ca3ac03526519
maxjiang216 <t63jiang@uwaterloo.ca>
2023-07-31 14:16:32 -0400

    fix file path name

--- scripts/bash/tourney.sh
+++ scripts/bash/tourney.sh
@@ -1,5 +1,5 @@
 dos2unix *
 # Build Cython module
 cd corintho_ai/python
-python3 tourney_setup.py build_ext --inplace -j 4
+python3 setup_tourney.py build_ext --inplace -j 4
 python3 tourney -p ../toml/tourney.toml -n ${1:-1}

ea2fbf797957550f11c71aac63b02fc12c5e9670
maxjiang216 <t63jiang@uwaterloo.ca>
2023-07-31 14:17:19 -0400

    Fix python file name

--- scripts/bash/tourney.sh
+++ scripts/bash/tourney.sh
@@ -2,4 +2,4 @@ dos2unix *
 # Build Cython module
 cd corintho_ai/python
 python3 setup_tourney.py build_ext --inplace -j 4
-python3 tourney -p ../toml/tourney.toml -n ${1:-1}
+python3 tourney.py -p ../toml/tourney.toml -n ${1:-1}

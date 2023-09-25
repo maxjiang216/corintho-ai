3abf0feab749549890dd8ca0f84ef26a214e7158
maxjiang216 <t63jiang@uwaterloo.ca>
2023-09-25 19:40:10 -0400

    fix r/w

--- corintho_ai/python/round.py
+++ corintho_ai/python/round.py
@@ -301,11 +301,11 @@ def write_ratings(folder, round_folder):
 
     # Get games
     games = {}
-    with open(os.path.join(round_folder, "get_games.txt"), "r") as f:
+    with open(os.path.join(round_folder, "get_games.txt"), "w+") as f:
         f.write(f"{folder}\n")
     for item in os.listdir(folder):
         item_path = os.path.join(folder, item)
-        with open(os.path.join(round_folder, "get_games.txt"), "r") as f:
+        with open(os.path.join(round_folder, "get_games.txt"), "w+") as f:
             f.write(f"{item_path}\n")
         if os.path.isdir(item_path):
             scores_file_path = os.path.join(item_path, "scores.txt")
@@ -318,7 +318,7 @@ def write_ratings(folder, round_folder):
                         ((int(score[0]), int(score[1])), float(score[2]))
                         for score in scores
                     ]
-                with open(os.path.join(round_folder, "get_games.txt"), "r") as f:
+                with open(os.path.join(round_folder, "get_games.txt"), "w+") as f:
                     f.write(f"{scores}\n")
                 for score in scores:
                     if score[0][0] not in games:

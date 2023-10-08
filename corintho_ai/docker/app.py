d11db2c681e6c89365a32cc00f5cd24e6574e23c
maxjiang216 <t63jiang@uwaterloo.ca>
2023-10-08 17:18:07 -0400

    lint

--- corintho_ai/docker/app.py
+++ corintho_ai/docker/app.py
@@ -1,7 +1,8 @@
+import os
+
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from play_corintho import choose_move
-import os
 
 app = Flask(__name__)
 CORS(app)
@@ -10,7 +11,7 @@ CORS(app)
 @app.route("/choose_move", methods=["POST"])
 def choose_move_route():
     data = request.get_json()
-    if data['password'] != os.environ.get('GCR_PASSWORD'):
+    if data["password"] != os.environ.get("GCR_PASSWORD"):
         return jsonify({"status": 401})
     return jsonify(
         choose_move(
